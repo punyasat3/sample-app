8fd990218c3c39eefb681dad9e44dfd3cc5bd489
Suresh Babu <41568630+suresh-devops@users.noreply.github.com>
2019-12-31 12:23:35 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,14 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# terraform {
-#   backend "s3" {
-#     bucket         = "tsureshbab-123"
-#     key            = "sample-app"
-#     region         = "us-eat-1"
-#     dynamodb_table = "terraform_locks"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "tsureshbab-123"
+    key            = "sample-app"
+    region         = "us-east-1"
+    dynamodb_table = "terraform_locks"
+  }
+}
 resource "aws_vpc" "myvpc" {
   cidr_block = "10.20.0.0/16"
   tags = {
