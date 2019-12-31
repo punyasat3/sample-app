2635c2fbd96c726e8e75afb9b5ad82d93bd630da
Suresh Babu <41568630+suresh-devops@users.noreply.github.com>
2019-12-31 15:20:21 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,7 +6,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket         = "tsureshbab-123"
-    key            = "sample-app"
+    key            = "sample-app1"
     region         = "us-east-1"
     dynamodb_table = "terraform_locks"
   }
