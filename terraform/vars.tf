353beb39be8fd0023b4f9c821b1565dc1fd42394
suresh <suresh@gmail.com>
2020-01-02 12:36:43 +0530

    typo

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -2,12 +2,34 @@ variable "profile" {}
 
 variable "env" {}
 
+variable "refion" {
+  default = "us-east-1"
+}
 variable "vpc_cidr" {
-  type = "map"
+  type = map
   default = {
     dev = "10.20.0.0/16"
     e2e = "10.21.0.0/16"
     qa  = "10.22.0.0/16"
+
+  }
+}
+
+variable "subnet_cidr" {
+  type = map
+  default = {
+    dev = "10.20.0.0/25"
+    e2e = "10.21.0.0/25"
+    qa  = "10.22.0.0/25"
+
+  }
+}
+variable "azs" {
+  type = map
+  default = {
+    dev = "us-east-1a"
+    e2e = "us-east-1b"
+    qa  = "us-east-1c"
 
   }
 }
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,7 +1,17 @@
 resource "aws_vpc" "myvpc" {
-  cidr_block = "${lookup(var.vpc_cidr, var.env)}"
+  cidr_block = lookup(var.vpc_cidr, var.env)
   tags = {
     Name = "MYVPC-${var.env}"
+  }
+
+}
+
+resource "aws_subnet" "mysubnet" {
+  cidr_block        = lookup(var.subnet_cidr, var.env)
+  vpc_id            = aws_vpc.myvpc.id
+  availability_zone = lookup(var.azs, var.env)
+  tags = {
+    Name = "Subnet-${var.env}"
   }
 
 }
