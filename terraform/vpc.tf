b561f4ac03cd87b4e129bf35f9415c1189af2c22
suresh <suresh@gmail.com>
2020-01-03 11:55:55 +0530

    typo

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -78,7 +78,7 @@ resource "aws_route_table" "private_rt" {
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = "${aws_nat_gateway.nat_gateway.id}"
+    nat_gateway_id = "${aws_nat_gateway.nat_gateway.*.id[0]}"
   }
 
   tags = {
