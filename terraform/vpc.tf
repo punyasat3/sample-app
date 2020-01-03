af99fff9a209a306cf617ce873af1ae0b7e1e809
suresh <suresh@gmail.com>
2020-01-03 09:35:41 +0530

    typo

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "myvpc" {
 
 resource "aws_subnet" "mysubnet" {
   count             = 3
-  cidr_block        = "${element(var.subnet_cidr, count.index, var.env)}"
+  cidr_block        = "${element([var.subnet_cidr, count.index], var.env)}"
   vpc_id            = aws_vpc.myvpc.id
   availability_zone = "${lookup(var.azs, var.env)}"
   tags = {
