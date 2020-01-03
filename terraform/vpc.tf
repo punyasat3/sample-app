a7ca3951cf9b305b2bbcd66305957f229cd47e01
suresh <suresh@gmail.com>
2020-01-03 11:14:54 +0530

    typo

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,13 +1,12 @@
+#######Declaring Local variables########
 locals {
   subnet_cidrs_list         = lookup(var.subnet_cidr, var.env)
   subnet_cidrs_list_private = lookup(var.subnet_cidr_private, var.env)
   vpc_id                    = aws_vpc.myvpc.id
 }
+#########################################
 
-
-
-
-
+#######VPC###############################
 resource "aws_vpc" "myvpc" {
   cidr_block           = lookup(var.vpc_cidr, var.env)
   enable_dns_hostnames = true
@@ -17,7 +16,9 @@ resource "aws_vpc" "myvpc" {
   }
 
 }
+#########################################
 
+###############Public Subnet#############
 resource "aws_subnet" "mysubnet" {
   count                   = "${length(local.subnet_cidrs_list)}"
   cidr_block              = "${element(local.subnet_cidrs_list, count.index)}"
@@ -30,7 +31,9 @@ resource "aws_subnet" "mysubnet" {
   }
 
 }
+#########################################
 
+###############Private Subnet#############
 resource "aws_subnet" "mysubnet_private" {
   count             = "${length(local.subnet_cidr_private)}"
   cidr_block        = "${element(local.subnet_cidrs_list_private, count.index)}"
@@ -41,3 +44,13 @@ resource "aws_subnet" "mysubnet_private" {
   }
 
 }
+#########################################
+
+###############IGW#######################
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = local.vpc_id
+  tags = {
+    Name = "MYVPC-IGW-${var.env}"
+  }
+}
+#########################################
