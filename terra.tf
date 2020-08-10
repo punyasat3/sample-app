6b8292ea5c100eaccd60658f4e0a7e641cb14124
punyasat3 <59074514+punyasat3@users.noreply.github.com>
2020-08-10 13:13:56 +0530

    Create terra.tf

---
+++ terra.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+        region = "us-west-2"
+}
+
+resource "aws_instance" "my-instance" {
+        ami = "ami-0873b46c45c11058d"
+        instance_type = "t2.micro"
+        key_name = "satya"
+        user_data = "${file("install_apache.sh")}"
+        tags = {
+                Name = "cms_api1"
+                Role = "cms_api"
+                Environmaent = "production"
+        }
+}
