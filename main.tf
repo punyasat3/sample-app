f0d49c42b41b933136e6a4e22d13ec28ed5bebc3
punyasat3 <59074514+punyasat3@users.noreply.github.com>
2020-08-23 22:27:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 provider "aws" {
         region = "us-west-2"
 }
-
+variable "ami_id" {
+  //description = "ami-0873b46c45c11058d"
+}
 resource "aws_instance" "my-instance" {
         ami = "${var.ami_id}"
         //ami = "ami-0873b46c45c11058d"
