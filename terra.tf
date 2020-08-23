96b85f5f66f55797e41d8a58e2c34a729c858967
punyasat3 <59074514+punyasat3@users.noreply.github.com>
2020-08-23 18:07:05 +0530

    Update terra.tf

--- terra.tf
+++ terra.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "my-instance" {
         key_name = "satya"
         user_data = "${file("install_apache.sh")}"
         tags = {
-                Name = "cms_apiiii"
+                Name = $name
                 Role = "cms_"
                 Environmaent = "production"
         }
