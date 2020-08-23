c425b60800bdf0ff639c722d086b5593ebf6f6db
punyasat3 <59074514+punyasat3@users.noreply.github.com>
2020-08-23 18:51:45 +0530

    Update terra.tf

--- terra.tf
+++ terra.tf
@@ -1,12 +1,9 @@
-locals {
-  is_t_instance_type = replace(var.instance_type, "/^t(2|3|3a){1}\\..*$/", "1") == "1" ? true : false
-}
 provider "aws" {
         region = "us-west-2"
 }
 
 resource "aws_instance" "my-instance" {
-        ami = var.ami
+        ami = "ami-0873b46c45c11058d"
         instance_type = "t2.micro"
         key_name = "satya"
         user_data = "${file("install_apache.sh")}"
