fddcb65f0902a3bc26be4b2f2e06aeaf1cffc2f2
punyasat3 <59074514+punyasat3@users.noreply.github.com>
2020-08-23 20:08:52 +0530

    Update terra.tf

--- terra.tf
+++ terra.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my-instance" {
-        ami = "${var.ami}"
+        ami = "${var.ami_id}"
         instance_type = "t2.micro"
         key_name = "satya"
         user_data = "${file("install_apache.sh")}"
