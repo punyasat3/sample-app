ff0b9ab9d4a93e72bde26494f71151e6dd23a767
punyasat3 <59074514+punyasat3@users.noreply.github.com>
2020-08-24 10:48:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ variable "instance_type" {}
 variable "subnet_id" {}
 variable "number_of_instances" {}
 variable "user_data" {}
-variable "vpc_id" {}
+//variable "vpc_id" {}
 
 
 provider "aws" {
@@ -13,7 +13,7 @@ resource "aws_instance" "my-instance" {
         ami = "${var.ami_id}"
         //ami = "ami-0873b46c45c11058d"
         instance_type = "t2.micro"
-        vpc           = "${var.vpc_id}"
+        subnet_id          = "${var.subnet_id}"
         key_name = "satya"
         user_data = "${file("install_apache.sh")}"
         tags = {
